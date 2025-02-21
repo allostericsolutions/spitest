6b27905e70f931a80ec6dd945f10e49fadaaa8de
Francisco Curiel de Icaza <ciclosporina2@hotmail.com>
2025-02-21 10:20:27 -0500

    Update navigation.py

--- components/navigation.py
+++ components/navigation.py
@@ -1,43 +1,43 @@
 import streamlit as st
 
 def unmark_question(index):
-    """ Funcion callback para desmarcar pregunta """
+    """ Callback function to unmark a question """
     st.session_state.marked.remove(index)
 
 def mark_current_question():
-    """ Funcion callback para marcar la pregunta actual"""
+    """ Callback function to mark the current question"""
     current_index = st.session_state.current_question_index
     st.session_state.marked.add(current_index)
-    st.success("Pregunta marcada para revisar.")
+    st.success("Question marked for review.") #Texto en ingles
 
 def display_navigation():
     """
-    Muestra tres botones para:
-    - Marcar y revisar la pregunta más tarde
-    - Mover a la pregunta anterior
-    - Mover a la siguiente pregunta
+    Displays three buttons for:
+    - Mark and review the question later
+    - Move to the previous question
+    - Move to the next question
     """
     col1, col2, col3 = st.columns(3)
 
     # Botón para marcar la pregunta
     with col1:
-        if st.button("Marcar para revisar", on_click=mark_current_question):
+        if st.button("Mark for review", on_click=mark_current_question): #Texto en ingles
             pass # Ya no es necesaria accion, el callback lo hace
 
     # Botón para ir a la pregunta anterior
     with col2:
-        if st.button("Anterior"):
+        if st.button("Previous"): #Texto en ingles
             if st.session_state.current_question_index > 0:
                 st.session_state.current_question_index -= 1
                 st.rerun() # Se añade rerun
             else:
-                st.warning("Esta es la primera pregunta.")
+                st.warning("This is the first question.") #Texto en ingles
 
     # Botón para ir a la pregunta siguiente
     with col3:
-        if st.button("Siguiente"):
+        if st.button("Next"): #Texto en ingles
             if st.session_state.current_question_index < len(st.session_state.selected_questions) - 1:
                 st.session_state.current_question_index += 1
                 st.rerun() #Se añade rerun
             else:
-                st.warning("Esta es la última pregunta.")
+                st.warning("This is the last question.")#Texto en ingles
