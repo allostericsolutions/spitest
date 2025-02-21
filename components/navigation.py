346e20f09387a6e5d0f306f975a01b9e18f40e6e
Francisco Curiel de Icaza <ciclosporina2@hotmail.com>
2025-02-21 10:03:02 -0500

    Update navigation.py

--- components/navigation.py
+++ components/navigation.py
@@ -1,5 +1,15 @@
 import streamlit as st
 
+def unmark_question(index):
+    """ Funcion callback para desmarcar pregunta """
+    st.session_state.marked.remove(index)
+
+def mark_current_question():
+    """ Funcion callback para marcar la pregunta actual"""
+    current_index = st.session_state.current_question_index
+    st.session_state.marked.add(current_index)
+    st.success("Pregunta marcada para revisar.")
+
 def display_navigation():
     """
     Muestra tres botones para:
@@ -11,16 +21,15 @@ def display_navigation():
 
     # Botón para marcar la pregunta
     with col1:
-        if st.button("Marcar para revisar"):
-            current_index = st.session_state.current_question_index
-            st.session_state.marked.add(current_index)
-            st.success("Pregunta marcada para revisar.")
+        if st.button("Marcar para revisar", on_click=mark_current_question):
+            pass # Ya no es necesaria accion, el callback lo hace
 
     # Botón para ir a la pregunta anterior
     with col2:
         if st.button("Anterior"):
             if st.session_state.current_question_index > 0:
                 st.session_state.current_question_index -= 1
+                st.rerun() # Se añade rerun
             else:
                 st.warning("Esta es la primera pregunta.")
 
@@ -29,5 +38,6 @@ def display_navigation():
         if st.button("Siguiente"):
             if st.session_state.current_question_index < len(st.session_state.selected_questions) - 1:
                 st.session_state.current_question_index += 1
+                st.rerun() #Se añade rerun
             else:
                 st.warning("Esta es la última pregunta.")
