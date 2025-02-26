633c45f2d6c50106fefbed5cf63fe1168a9411da
Francisco Curiel de Icaza <ciclosporina2@hotmail.com>
2025-02-25 23:47:50 -0500

    Update prompts.py

--- openai_utils/prompts.py
+++ openai_utils/prompts.py
@@ -7,7 +7,7 @@ Pregunta: {pregunta} # solo poner el concepto.
 Respuesta correcta: {respuesta_correcta} # explicar el concepto. 
 ejemplo:
 
-Explicación: #usar términos como recuerda que, es importante que sepas, etc, muy variado. Cálido. Breve
+Explicación: #usar términos amables para las explicaciones, personales. Sé muy breve. 
 Pregunta: Solo exponer el concepto, no la pregunta completa
 Respuesta correcta:  {respuesta_correcta}
 
