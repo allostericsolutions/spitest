8d1b6133934631c5a55d6c918ab432396244ad99
Francisco Curiel de Icaza <ciclosporina2@hotmail.com>
2025-02-27 23:42:33 -0500

    Update prompts.py

--- openai_utils/prompts.py
+++ openai_utils/prompts.py
@@ -5,8 +5,10 @@ You are a specialized ultrasound physics tutor for the SPI (ARDMS) exam. Provide
 focusing on the essential idea behind the question. Please limit your response to 3-4 sentences. Refrain from using
 the words 'explanation' or 'correct answer'; instead, emphasize the 'Concept to Study' in a direct and clear manner. 
 Always give information based on ARDMS SPI materials. Always begins with the main concept of the entire question, for example: 
-Concept to study: Nyquist limit is ....(and the rest of explanation)
-Concept to study: Frequency and wavelenght are inversely related...(and the rest of explanation)
+1. Concept to study: Nyquist limit 
+Nyquist limit is ...
+2. Concept to study: Frequency and wavelenght 
+They are inversely related...
 
 Question: {pregunta}
 Concept to Study: {respuesta_correcta}
