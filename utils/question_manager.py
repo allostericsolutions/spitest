430aae756e7bddf6338dcd17049ea7c146d8006f
Francisco Curiel de Icaza <ciclosporina2@hotmail.com>
2025-02-28 01:05:14 -0500

    Update question_manager.py

--- utils/question_manager.py
+++ utils/question_manager.py
@@ -16,17 +16,17 @@ def select_random_questions(total=120):
     preguntas = load_questions()
     classification_percentages = {
         "Basic Physics (Frequency, Wavelength, Period, and Propagation)": 6,
-        "Instruments (Transducers Construction and function; Image Settings and Display)": 4,
-        "Doppler Physics and Instrumentation": 17,
+        "Instruments (Transducers Construction and function; Image Settings and Display)": 7,
+        "Doppler Physics and Instrumentation": 19,
         "Wave Properties and Interactions with matter": 8,
-        "Bioeffects, Spatial, Temporal Resolution": 3,
+        "Bioeffects, Spatial, Temporal Resolution": 5,
         "Safety and Risk Management and new tech": 3,
         "Basic Physics Concepts, Wave Parameters, and Attenuation": 6,
         "Transducers, Resolution, and Image Formation.": 9,
         "Image Display, Processing, and Artifacts": 16,
-        "Hemodynamics and Doppler Principles": 17,
-        "Quality Assurance and Quality Control": 4,
-        "Patient care and new technology": 7
+        "Hemodynamics and Doppler Principles": 15,
+        "Quality Assurance and Quality Control": 3,
+        "Patient care and new technology": 3
     }
     total_percentage = sum(classification_percentages.values())
     if total_percentage != 100:
