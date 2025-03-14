5d84edbb4734928694ac6cb2aac68aacf17b9f54
Francisco Curiel de Icaza <ciclosporina2@hotmail.com>
2025-03-14 14:58:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -158,7 +158,7 @@ def exam_screen():
             st.session_state.confirm_finish = False
 
         with st.form("finish_form"):
-            st.warning("Are you sure you want to finish the exam?")
+            #st.warning("Are you sure you want to finish the exam?")
             if st.form_submit_button("Confirm Completion"):
                 st.session_state.confirm_finish = True
 
